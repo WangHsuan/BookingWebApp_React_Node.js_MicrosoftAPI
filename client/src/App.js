83ca962ebfb81a71828a48038f16db825a11a081
wanghsuan <wanghsuanqut@gmail.com>
2020-02-10 09:53:45 +1000

    .env and firebase

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Customers from './components/customers/customers';
 import Calendar from './components/calendar/calendar';
 
 function App() {
--- server.js
+++ server.js
@@ -1,22 +1,37 @@
 const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
+require('dotenv/config');
+var bodyParser = require('body-parser')
 
-app.get('/api/customers',(req,res)=>{
-    const customers=[
-        {id:1, firstName:"Hsuan",lastName:"Wang"},
-        {id:2, firstName:"Jenny", lastName:"Hsieh"},
-        {id:3, firstName:"Charles", lastName:"Hsieh"}
-    ]
-
-    res.json(customers);
+//firebase--------------------------
+var admin = require("firebase-admin");
+var serviceAccount = require("./bookingweb-7c118-firebase-adminsdk-w97ed-89b70c4481.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://bookingweb-7c118.firebaseio.com"
+});
+//test
+var fireData = admin.database();
+//read
+fireData.ref('todos').once('value', function(snapshot){
+    //console.log(snapshot.val());
 })
+//write
+fireData.ref('todos').set({'title':'welcome to real world'})
+.then(fireData.ref('todos').once('value', function(snapshot){
+    console.log(snapshot.val());
+}))
+//-------------------------------------
 
+// 增加 body 解析
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
 
 //postman to get calendar id
-const CalendarID = "AQMkADMyZDg4AGU2OC05ZDUyLTQ1NzktYTIzNS1mOTYzMGNkOTFkMTkARgAAA-ZKmLIVon9Hm8YUE3_34WEHAOTndrKUfs5EvqsgdiNCkCYAAAIBBgAAAOTndrKUfs5EvqsgdiNCkCYAAAIdEQAAAA==";
-const clientID = "7d0a57c8-6f93-425d-9eb0-83a45a401277";
-const clientSecret = "bXW4wqCkIX4*2N3rCD*NMpwrttBKxy-.";
+const CalendarID = process.env.CalendarID;
+const clientID = process.env.clientID;
+const clientSecret = process.env.clientSecret;
 
 //get eveny day
 const currentDay = new Date();
@@ -25,7 +40,7 @@ const currentDate = currentDay.getDate();
 const startDate='2020-01-20T01:00:00'
 const endDate = '2020-12-20T01:00:00';
 
-
+//Get Calendar Data From Microsoft and then allows React to fetch
 app.get('/api/getEvents',(req,res)=>{
 
     //fetch microsoft calendar events
@@ -66,10 +81,76 @@ app.get('/api/getEvents',(req,res)=>{
 
 })
 
-// app.post('/api/postEvents',(req,res)=>{
-//     res.json(req);
+//Get data from React and then post to Microsoft Outlook
+app.post('/api/postEvents',(req,res)=>{
+    
+    const content = req.body.content;
+    //---------------------------------------
+    if(content.Time.slice(9,11)==='30'){
+        console.log(content.Time.slice(6,8))
+        var addHour = Number(content.Time.slice(6,8))+1
+        var timeToggle = '00'
+        }
+        else{
+        var addHour = Number(content.Time.slice(6,8))
+        timeToggle = '30'
+        }
+    
+    var postData = {
+        "Subject": `${content.Topic}`,
+        "Body": {
+        "ContentType": "HTML",
+        "Content": `${content.Content}`
+        },
+        "Start": {
+            "DateTime": `2019-${content.Time.slice(0,5)}T${content.Time.slice(6,11)}:00`,
+            "TimeZone": "E. Australia Standard Time"
+        },
+        "End": {
+            "DateTime":`2019-${content.Time.slice(0,5)}T${addHour}:${timeToggle}:00`,
+            "TimeZone": "E. Australia Standard Time"
+        },
+        "Attendees": [
+        {
+            "EmailAddress": {
+            "Address": `${content.Studnet_Email}`,
+            "Name": "Janet Schorr"
+            },
+            "Type": "Required"
+        }
+        ]
+        }  
+    console.log(postData);
+    //----------------------------------
+//     fetch("https://login.microsoftonline.com/WangHsuan.onmicrosoft.com/oauth2/token", { method: 'POST',headers: {'Content-Type': 'application/x-www-form-urlencoded'}, 
+//     body: `client_id=${clientID}&client_secret= ${clientSecret}&grant_type=client_credentials & resource=https://graph.microsoft.com`  })
+//     .then(response => response.json())
+//     .then(data =>{ 
+    
+// // console.log(postData);
+// //PostEvent--------------------------
+//   fetch(` https://graph.microsoft.com/beta/users/WangHsuan@WangHsuan.onmicrosoft.com/calendars/AQMkADMyZDg4AGU2OC05ZDUyLTQ1NzktYTIzNS1mOTYzMGNkOTFkMTkARgAAA-ZKmLIVon9Hm8YUE3_34WEHAOTndrKUfs5EvqsgdiNCkCYAAAIBBgAAAOTndrKUfs5EvqsgdiNCkCYAAAIdEQAAAA==/events` ,{
+//        method:'POST',
+//        body:JSON.stringify(postData),
+//        headers: {
+//          'Authorization': `bearer ${data.access_token}`,
+//          'Content-Type':'application/json',
+//      }
+//     })
+//       .then(function(response) {
+
+//             return response.json();
+//             })
+//       .then(function(myJson) {
+//         console.log(myJson)
+//         res.json(myJson)
+//       })
+
+//    } ) // expecting a json response
+
+})
    
-// })
+
 
 const port = 5000;
 
